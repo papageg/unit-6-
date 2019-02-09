9257e898268601cde22df73d250930c17a9547f6
Greg <greggordon98@gmail.com>
2019-02-08 19:28:31 -0800

    aaa
    aaa

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const { projects } = require('./data.json');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const projectsRoutes = require('./routes/projects');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 app.use('/projects', projectsRoutes);
 
-app.use('/', (res, req, next) => {
+
+
+app.get('/', (req, res, next) => {
     res.render('index', { projects });
+  
 });
 
-app.use('/about', (res, req, next) => {
+app.get('/about', (req, res, next) => {
     res.render('about');
 });
 
+app.use((req, res, next) => {
+    console.log("A Error has happened!");
+    const err = new Error('Error!!!');
+    err.status = 500; 
+    next(err);
+});
 
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
   });
+  module.exports = app;
---
+++ routes/projects.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var router = express.Router();
+const { projects } = require('../data.json');
+module.exports = router; 
